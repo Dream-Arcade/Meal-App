96319aca4e5c66999e1b0caa74203cf432f5e6a4
Dream-Arcade <thinkingsingularity@gmail.com>
2023-12-03 11:03:59 -0600

    Added logic

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverViewScreen({ navigation, route }) {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealsItem) => {
+    return mealsItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View>
-      <Text>Meals Overview Screen {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
